4eb8f4be9765689261575b0e8671ee01c743d6cf
DelaramSheikhbahaie <sheikhbahaiework@gmail.com>
2025-01-25 23:33:20 +0100

    delete unwanted files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,6 @@ export default defineNuxtConfig({
         { name: 'description', content: appDescription },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
         { name: 'theme-color', media: '(prefers-color-scheme: light)', content: 'white' },
-        { name: 'theme-color', media: '(prefers-color-scheme: dark)', content: '#222222' },
       ],
     },
   },
