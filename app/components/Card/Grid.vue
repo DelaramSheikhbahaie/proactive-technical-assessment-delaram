8cd71757d9fdbbdbbbae7fa49195bcf3ce294c8a
DelaramSheikhbahaie <sheikhbahaiework@gmail.com>
2025-01-26 12:21:57 +0100

    create grid and list cards

---
+++ app/components/Card/Grid.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+    character: any
+    detailsLink: any
+}>()
+const rickAndMortyStore = useRickAndMortyStore()
+</script>
+<template>
+    <div class="w-full bg-white p-4 my-2 flex flex-col items-start rounded-lg">
+        <NuxtImg :src="character.image || '/images/default-avatar.jpg'" alt="Character avatar"
+            class="w-full mb-4 rounded-lg" />
+            <p class="mb-4">{{ character.name }}</p>
+        <NuxtLink :to="detailsLink" class="self-end">
+            <UIcon name="i-material-symbols:arrow-circle-right-rounded" class="w-12 h-12 bg-pink-500" />
+        </NuxtLink>
+    </div>
+</template>
---
+++ app/components/Card/List.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+    character: any
+    detailsLink: any
+}>()
+const rickAndMortyStore = useRickAndMortyStore()
+</script>
+<template>
+    <div class="w-full bg-white p-4 my-2 flex flex-row items-center justify-between rounded-lg">
+        <div class="flex flex-row justify-start items-center">
+            <NuxtImg :src="character.image || '/images/default-avatar.jpg'" alt="Character avatar"
+                class="w-24 h-24 mr-8 rounded-lg" />
+            <p>{{ character.name }}</p>
+        </div>
+        <NuxtLink :to="detailsLink">
+            <UIcon name="i-material-symbols:arrow-circle-right-rounded" class="w-12 h-12 bg-pink-500" />
+        </NuxtLink>
+    </div>
+</template>
