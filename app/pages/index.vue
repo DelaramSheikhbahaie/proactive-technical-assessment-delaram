8adfc34f553c478a2fc8faa14749fb683e2d9eb9
DelaramSheikhbahaie <sheikhbahaiework@gmail.com>
2025-01-26 15:50:59 +0100

    create home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <UContainer class="bg-purple-200 text-black w-5/6 p-8 flex justify-start items-start flex-col rounded-lg my-16">
-     Home
+  <UContainer class="bg-purple-200 text-black w-5/6 p-8 flex justify-center items-center flex-col rounded-lg my-16">
+    <h1 class="text-xl mb-8">Please select a page</h1>
+    <div class="flex flex-row justify-between">
+      <NuxtLink to="rickandmorty">
+        <UButton color="pink" variant="solid" class="py-4 px-8 mr-4">Rick And Morty Page</UButton>
+      </NuxtLink>
+      <NuxtLink to="pokemon">
+        <UButton color="pink" variant="solid" class="py-4 px-8 mr-4">Pokemon Page</UButton>
+      </NuxtLink>
+    </div>
   </UContainer>
 </template>
